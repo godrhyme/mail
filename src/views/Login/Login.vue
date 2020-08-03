f55e148b7a978f5f1b2aae1406be2436ff36100f
godrhyme <984983605@qq.com>
2020-08-03 10:08:38 +0800

    电商项目初始文件，测试

---
+++ src/views/Login/Login.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <span data-v-ac2576ae="" class="icon icon-goback"></span>
+    <span data-v-ac2576ae="" class="text-header">京东登录注册</span>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Login'
+}
+</script>
+
+<style scoped>
+
+</style>
